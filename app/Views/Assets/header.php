aadb95e2847dcc896908811974bf984f8bbdbccd
siubhans <103741645+siubhansm@users.noreply.github.com>
2022-08-21 08:51:46 +0100

    brand logo

--- app/Views/Assets/header.php
+++ app/Views/Assets/header.php
@@ -14,6 +14,9 @@
   
     <nav class="navbar navbar-expand-lg bg-light">
   <div class="container">
+     <a class="navbar-brand" href="/login">
+      <img src="favicon.ico" alt="" width="30" height="24">
+    </a>
     <a class="navbar-brand" href="#">Yoga Hub</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
