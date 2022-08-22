03fd840cd18068256df9186b526df8f4c6764fb3
siubhans <103741645+siubhansm@users.noreply.github.com>
2022-08-22 11:33:56 +0100

    Update classes.php

--- app/Views/Classes/classes.php
+++ app/Views/Classes/classes.php
@@ -2,20 +2,20 @@
     <body class="nBody">
      <?php echo view ('Assets/header'); 
       ?>
-
-      <div class="container d-flex flex-wrap justify-content-around">
+  <div class="container">
+      <div class="d-flex flex-wrap justify-content-around">
 
           <?php foreach($table as $key){ ?>
             <div >
-              <h3><?php echo $key['name']; ?></h3>
+              <h3 class="mx-auto"><?php echo $key['name']; ?></h3>
               <br>
-             <iframe width="480" height="270" src="<?php echo $key['embedURL']; ?>" title="<?php echo $key['name']; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-             <br><p><?php echo $key['description']; ?></p>
+             <iframe class="mx-auto"width="480" height="270" src="<?php echo $key['embedURL']; ?>" title="<?php echo $key['name']; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+             <br><p class="mx-auto"><?php echo $key['description']; ?></p>
         </div>
  
           <?php } ?>
 
-         
+         </div>
 </div>
 <div class="blankSpace"></div>
  <?php echo view ('Assets/footer');  ?>
